1d46e4e81d6fed9449a89a756c4358a8c10f80cb
thyneb19 <thyne.boonmark@gmail.com>
2019-11-22 15:49:05 -0800

    fixed data labels

--- CyberSecurity_DataExploration_App/app.R
+++ CyberSecurity_DataExploration_App/app.R
@@ -8,6 +8,9 @@
 #
 
 library(shiny)
+library(ggplot2)
+breaches <- readRDS("C:/Users/thyne/Documents/GitHub/Info202-Final-Project/CyberSecurity_DataExploration_App/data/Breach_Data.RDS")
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -18,11 +21,8 @@ ui <- fluidPage(
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+        checkboxGroupInput("somevalue", "Some value", choices = unique(breaches$Sector), FALSE),
+        verbatimTextOutput("value")
       ),
       
       # Show a plot of the generated distribution
@@ -36,12 +36,7 @@ ui <- fluidPage(
 server <- function(input, output) {
    
    output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
       
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
    })
 }
 
