f1565434f1285033e852671723087099d1f991c3
thyneb19 <thyne.boonmark@gmail.com>
2019-11-25 12:27:07 -0800

    re-added app file

---
+++ CyberSecurity_DataExploration_App/app.R
@@ -0,0 +1,45 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggplot2)
+breaches <- readRDS("C:/Users/thyne/Documents/GitHub/Info202-Final-Project/CyberSecurity_DataExploration_App/data/cleanedBreachData.RDS")
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Old Faithful Geyser Data"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+        checkboxGroupInput("somevalue", "Some value", choices = unique(breaches$Sector), FALSE),
+        verbatimTextOutput("value")
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("distPlot")
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$distPlot <- renderPlot({
+     ggplot(data2, aes(x = Sector, y = Records_Lost, fill = Method)) + geom_bar(stat='identity')
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
